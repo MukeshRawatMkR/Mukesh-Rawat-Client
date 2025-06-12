645a104003ed3e6502a68d5846ea41202f40ac61
mukeshrawatmkr <swissmukeshrawat@gmail.com>
2025-06-12 09:22:46 -0700

    Setting up tailwindCSS

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,7 @@ const Header = () => {
     <div className="flex flex-col items-center justify-center h-full">
       <h1 className="text-4xl font-bold mb-4">Welcome to the Client App</h1>
       <p className="text-lg text-gray-700">This is the body of the application.</p>
-    </div>
-  );
+    </div>  );
 };
 
 export default Header;
