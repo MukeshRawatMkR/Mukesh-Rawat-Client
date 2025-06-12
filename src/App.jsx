4d46773f04dc34140ce299482258fc980edddba6
mukeshrawatmkr <swissmukeshrawat@gmail.com>
2025-06-12 08:47:02 -0700

    Done some refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Header from './components/Header';
+import Body from './components/Body';
 import './App.css'
 function App() {
 
   return (
     <>
-      <h1>Mukesh Rawat</h1>
+      <Header/>
     </>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+
+const Header = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-full">
+      <h1 className="text-4xl font-bold mb-4">Welcome to the Client App</h1>
+      <p className="text-lg text-gray-700">This is the body of the application.</p>
+    </div>
+  );
+};
+
+export default Header;
